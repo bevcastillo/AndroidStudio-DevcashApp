2520f688e40e0d58b534ec0ec51433b0454aca58
Beverly May Castillo <bevcastillo>
2019-05-07 18:16:39 +0800

    Added splash screen

--- app/src/main/java/com/example/devcash/SplashScreenActivity.java
+++ app/src/main/java/com/example/devcash/SplashScreenActivity.java
@@ -1,13 +1,70 @@
 package com.example.devcash;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.security.acl.Owner;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        
+        ///
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent login = new Intent(SplashScreenActivity.this, OwnerLoginActivity.class);
+                startActivity(login);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        
+        super.onWindowFocusChanged(hasFocus);
+        
+        if(hasFocus){
+            hideSystemUI();
+        }
+    
+    }
+
+    private void hideSystemUI() {
+        //enables regular immersive mode
+        //for "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE,
+        //or for "sticky immersive", replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                //set the content to appear under the system bars so that the
+                //content doesn't resize when the system bars hide and show.
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+                // hide the nav bar and status bar
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN );
+    }
+
+    //shows the system bars by removing all the flags except
+    // for the ones that make the content appear under the system bars
+    private void showSystemUI(){
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN );
     }
 }
