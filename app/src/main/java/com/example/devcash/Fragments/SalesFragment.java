53fe7ff91f35711558b7dc530cda7ec8fac5eec9
Beverly May Castillo <beverlymaycastillo@gmail.com>
2019-05-24 11:23:52 +0800

    added sales purchase fragments

--- app/src/main/java/com/example/devcash/Fragments/SalesFragment.java
+++ app/src/main/java/com/example/devcash/Fragments/SalesFragment.java
@@ -27,7 +27,8 @@ public class SalesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sales, container, false);
+        View view = inflater.inflate(R.layout.fragment_sales, container, false);
+        return view;
     }
 
     @Override
