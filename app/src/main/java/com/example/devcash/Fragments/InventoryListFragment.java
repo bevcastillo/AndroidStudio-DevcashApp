b596badf2f7ffccf828ea1842337aec19f6d358c
Beverly May Castillo <beverlymaycastillo@gmail.com>
2019-07-06 23:21:15 +0800

    changed listview to recyclerview in inventory options

--- app/src/main/java/com/example/devcash/Fragments/InventoryListFragment.java
+++ app/src/main/java/com/example/devcash/Fragments/InventoryListFragment.java
@@ -2,6 +2,7 @@ package com.example.devcash.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.LongDef;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,14 +10,18 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.LoginFilter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.devcash.CustomAdapters.InventoryListAdapter;
 import com.example.devcash.InventoryRecyclerViewAdapter;
+import com.example.devcash.InventoryRecyclerViewDataList;
 import com.example.devcash.Model.InventoryList;
 import com.example.devcash.R;
 import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
@@ -28,6 +33,13 @@ import java.util.ArrayList;
  * A simple {@link Fragment} subclass.
  */
 public class InventoryListFragment extends Fragment {
+
+    private static final String TAG = "InventoryListFragment";
+    //variables
+    private ArrayList<Integer> mIcon = new ArrayList<>();
+    private ArrayList<String> mLabel = new ArrayList<>();
+
+//        implements InventoryRecyclerViewAdapter.OnInventoryListener{
 
 //    ListView lvinventory;
 //    ArrayList<InventoryList> list = new ArrayList<InventoryList>();
@@ -44,27 +56,46 @@ public class InventoryListFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_inventory_list, container, false);
 
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.inventorylist_recycleview);
-        recyclerView.addItemDecoration(new HorizontalDividerItemDecoration.Builder(getActivity())
-                .build()); //adding a divider into the recyclerview list
-
-        InventoryRecyclerViewAdapter adapter = new InventoryRecyclerViewAdapter();
-        recyclerView.setAdapter(adapter);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(layoutManager);
-
-//        lvinventory = (ListView) view.findViewById(R.id.inventorylist_listview);
-//        adapter = new InventoryListAdapter(getActivity(),list);
+        Log.d(TAG, "onCreateView: started");
+        initImageBitmaps();
 //
-//        list.add(new InventoryList(R.drawable.ic_product,"Products"));
-//        list.add(new InventoryList(R.drawable.ic_services,"Services"));
-//        list.add(new InventoryList(R.drawable.ic_category, "Categories"));
-//        list.add(new InventoryList(R.drawable.ic_local_offer,"Discounts"));
+//        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.inventorylist_recycleview);
+//        recyclerView.addItemDecoration(new HorizontalDividerItemDecoration.Builder(getActivity())
+//                .build()); //adding a divider into the recyclerview list
 //
-//        lvinventory.setAdapter(adapter);
-//        lvinventory.setOnItemClickListener(this);
+//        InventoryRecyclerViewAdapter adapter = new InventoryRecyclerViewAdapter();
+//        recyclerView.setAdapter(adapter);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+//        recyclerView.setLayoutManager(layoutManager);
 
         return view;
+    }
+
+    private void initImageBitmaps(){
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps");
+
+        mIcon.add(R.drawable.ic_product);
+        mLabel.add("Products");
+
+        mIcon.add(R.drawable.ic_customer);
+        mLabel.add("Services");
+
+        mIcon.add(R.drawable.ic_category);
+        mLabel.add("Categories");
+
+        mIcon.add(R.drawable.ic_tag);
+        mLabel.add("Discounts");
+
+        initRecyclerView();
+
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview");
+        RecyclerView recyclerView = getView().findViewById(R.id.inventorylist_recycleview);
+        InventoryRecyclerViewAdapter adapter = new InventoryRecyclerViewAdapter(getActivity(), mIcon, mLabel);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 
     @Override
@@ -73,6 +104,44 @@ public class InventoryListFragment extends Fragment {
 
 
     }
+
+//    //adding a listener to the recycleview list
+//    @Override
+//    public void onInventoryClick(int position) {
+//
+//        FragmentManager fragmentManager = getFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//        switch (position){
+//            case 0:
+//                ProductsFragment productsFragment = new ProductsFragment();
+//                fragmentTransaction.replace(R.id.inventorylist_fragmentcontainer, productsFragment);
+//                fragmentTransaction.addToBackStack(null);
+//                fragmentTransaction.commit();
+//                break;
+//            case 1:
+//                ServicesFragment servicesFragment = new ServicesFragment();
+//                fragmentTransaction.replace(R.id.inventorylist_fragmentcontainer, servicesFragment);
+//                fragmentTransaction.addToBackStack(null);
+//                fragmentTransaction.commit();
+//                break;
+//            case  2:
+//                CategoriesFragment categoriesFragment = new CategoriesFragment();
+//                fragmentTransaction.replace(R.id.inventorylist_fragmentcontainer, categoriesFragment);
+//                fragmentTransaction.addToBackStack(null);
+//                fragmentTransaction.commit();
+//                break;
+//            case 3:
+//                DiscountsFragment discountsFragment = new DiscountsFragment();
+//                fragmentTransaction.replace(R.id.inventorylist_fragmentcontainer, discountsFragment);
+//                fragmentTransaction.addToBackStack(null);
+//                fragmentTransaction.commit();
+//                break;
+//        }
+//
+//
+//    }
+
 
 //    @Override
 //    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
--- app/src/main/java/com/example/devcash/InventoryRecyclerViewAdapter.java
+++ app/src/main/java/com/example/devcash/InventoryRecyclerViewAdapter.java
@@ -1,53 +1,140 @@
 package com.example.devcash;
 
+import android.content.Context;
+import android.gesture.GestureLibraries;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import org.w3c.dom.Text;
 
-public class InventoryRecyclerViewAdapter extends RecyclerView.Adapter {
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.customlayout_inventorylist, viewGroup, false);
-        return new InvRecycleViewHolder(view);
+import java.util.ArrayList;
+
+public class InventoryRecyclerViewAdapter extends RecyclerView.Adapter<InventoryRecyclerViewAdapter.ViewHolder>{
+
+    private static final String TAG = "RecyclerViewAdapter";
+    private ArrayList<Integer> mIcon = new ArrayList<>();
+    private ArrayList<String> mLabel = new ArrayList<>();
+    private Context context;
+
+    public InventoryRecyclerViewAdapter(Context context, ArrayList<Integer> mIcon, ArrayList<String> mLabel) {
+        this.mIcon = mIcon;
+        this.mLabel = mLabel;
+        this.context = context;
     }
 
+    //responsible for inflating the view
+    @NonNull
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-        ((InvRecycleViewHolder)viewHolder).bindView(i);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.customlayout_inventorylist, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
+
+        @Override
+        public void onBindViewHolder(@NonNull InventoryRecyclerViewAdapter.ViewHolder viewHolder, final int i) {
+            Log.d(TAG, "onBindViewHolder: called.");
+            Glide.with(context)
+                    .asBitmap()
+                    .load(mIcon.get(i))
+                    .into(viewHolder.icon);
+
+
+            viewHolder.label.setText(mLabel.get(i));
+            viewHolder.customLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "OnClick: clicked on: " + mLabel.get(i));
+                    Toast.makeText(context, mLabel.get(i), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
 
     @Override
     public int getItemCount() {
-        return InventoryRecyclerViewDataList.label.length;
+        return mLabel.size();
     }
 
-    private class InvRecycleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private ImageView mPicturePath;
-        private TextView mLabel;
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView icon;
+        TextView label;
+        LinearLayout customLayout;
 
-        public InvRecycleViewHolder(View itemView){
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            mPicturePath = (ImageView) itemView.findViewById(R.id.inventorylist_icon);
-            mLabel = (TextView) itemView.findViewById(R.id.inventorylist_title);
-            itemView.setOnClickListener(this);
-        }
-
-        //onlick listener for itemview
-        @Override
-        public void onClick(View v) {
-
-        }
-
-        public void bindView(int position){
-            mPicturePath.setImageResource(InventoryRecyclerViewDataList.picturePath[position]);
-            mLabel.setText(InventoryRecyclerViewDataList.label[position]);
+            icon = itemView.findViewById(R.id.inventorylist_icon);
+            label = itemView.findViewById(R.id.inventorylist_title);
+            customLayout = itemView.findViewById(R.id.inventoryoptions_layout);
         }
     }
+
+////    private OnInventoryListener mOnInventoryListener;
+//
+//    @NonNull
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+//        View view = LayoutInflater.from(viewGroup.getContext())
+//                .inflate(R.layout.customlayout_inventorylist, viewGroup, false);
+//
+////        return new InvRecycleViewHolder(view, mOnInventoryListener);
+//    }
+
+//    @Override
+//    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+//        ((InvRecycleViewHolder)viewHolder).bindView(i);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return InventoryRecyclerViewDataList.label.length;
+//    }
+//
+//    //adding listeners
+//    private class InvRecycleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+//        private ImageView mPicturePath;
+//        private TextView mLabel;
+//        private View v;
+//
+//        //added for the onInventoryListener
+//        OnInventoryListener onInventoryListener;
+//
+//        public InvRecycleViewHolder(View itemView, OnInventoryListener onInventoryListener){ //onInventoryListener is added
+//            super(itemView);
+//            mPicturePath = (ImageView) itemView.findViewById(R.id.inventorylist_icon);
+//            mLabel = (TextView) itemView.findViewById(R.id.inventorylist_title);
+//
+//            //added for the onInventoryListener
+//            this.onInventoryListener = onInventoryListener;
+////            v = itemView;
+//
+//    //            itemView.setOnClickListener(this);
+//        }
+//
+//        //onlick listener for itemview
+//        @Override
+//        public void onClick(View v) {
+//            //add some code here..
+//            onInventoryListener.onInventoryClick(getAdapterPosition());
+//        }
+//
+//        public void bindView(int position){
+//            mPicturePath.setImageResource(InventoryRecyclerViewDataList.picturePath[position]);
+//            mLabel.setText(InventoryRecyclerViewDataList.label[position]);
+//        }
+//    }
+//
+//    public interface OnInventoryListener{
+//        void onInventoryClick(int position);
+//    }
 }
